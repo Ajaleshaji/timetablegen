660bb3bd639a4fa5e4ebe886fa83cf4686cd2020
Ajaleshaji <ajaleshbabu@gmail.com>
2025-05-20 09:36:27 +0530

    change dep

--- client/src/pages/TimetablePage.jsx
+++ client/src/pages/TimetablePage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import "../styles/TimetablePage.css"
 import { useNavigate } from "react-router-dom";
 
 const TimetablePage = () => {
